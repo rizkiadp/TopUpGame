fac80cd25c6e995e47044b7a65b575e92e7cc009
My Computer <My Computer@LAPTOP-B8L8P8B9>
2022-11-22 11:37:43 +0700

    Add project files.

---
+++ TopUpGame/ListGame.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TopUpGame
+{
+    /// <summary>
+    /// Interaction logic for ListGame.xaml
+    /// </summary>
+    public partial class ListGame : Window
+    {
+        public ListGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PilihDmML ListGame = new PilihDmML();
+            ListGame.Show();
+        }
+    }
+}
